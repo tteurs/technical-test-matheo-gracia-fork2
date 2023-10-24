d73a7b63a4bf171c2161a60fffe2dd89e046f45b
Matheo Gracia Pegoraro <tteurs@gmail.com>
2023-10-23 23:24:10 -0300

    add Seeds to project

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "… Seeding data …"
+# Create 5 movies to add to DB
+5.times { Movie.create(title: Faker::Movie.title,
+                        release_date: Faker::Date.between(from: 2.years.ago, to: Date.today),
+                        genre: Faker::Gender.type ,
+                        runtime: '2hours' ,
+                        parental_rating: 'PG13',
+                        plot: Faker::Lorem.sentence
+) }
+
+#add Ratings to the movies
+Movie.all.each do |movie|
+  5.times { Rating.create(movie_id: movie.id, grade: Faker::Number.between(from: 1, to: 5)) }
+end
