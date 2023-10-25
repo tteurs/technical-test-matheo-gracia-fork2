d8cac4cdfea03699e2d05af925b8ef35e82996c2
Matheo Gracia Pegoraro <tteurs@gmail.com>
2023-10-25 01:50:43 -0300

    add .env for react app

--- movies-app/src/services/api.js
+++ movies-app/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api/v1',
+  baseURL: {import.meta.env.REACT_APP_API_URL_PROD || 'http://localhost:3001'},
 });
 
 const STATS_ENDPOINTS = {
