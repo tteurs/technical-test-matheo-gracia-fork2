ba8abbafd8613db0d5a5e88ee9b510ffb1d03240
Matheo Gracia Pegoraro <tteurs@gmail.com>
2023-10-24 23:49:21 -0300

    improve view data

--- db/migrate/20231024045528_create_year_with_most_movies_view.rb
+++ db/migrate/20231024045528_create_year_with_most_movies_view.rb
@@ -2,7 +2,7 @@ class CreateYearWithMostMoviesView < ActiveRecord::Migration[6.1]
   def up
     execute <<-SQL
       CREATE VIEW year_with_most_movies AS
-      SELECT EXTRACT(YEAR FROM release_date) AS release_year, COUNT(*) AS movie_count
+      SELECT CAST(EXTRACT(YEAR FROM release_date) AS INTEGER) AS release_year, COUNT(*) AS movie_count
       FROM movies
       GROUP BY release_year
       ORDER BY movie_count DESC
