b60becb8c708587daf58a2eccae0afc0bf1ae424
Matheo Gracia Pegoraro <tteurs@gmail.com>
2023-10-28 17:42:14 -0300

    Add style to movie details and vote function.

--- movies-app/src/components/MovieDetails.js
+++ movies-app/src/components/MovieDetails.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import { getMovie, deleteMovie } from '../services/api';
+import { useParams, useNavigate, Link } from 'react-router-dom';
+import { getMovie, deleteMovie, rateMovie } from '../services/api';
+import { Button, Card, CardBody, CardText, CardTitle, ListGroup, Container, Row} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const MovieDetails = () => {
   const { id } = useParams();
   const [movie, setMovie] = useState(null);
+  const [userRating, setUserRating] = useState(null);
   const navigate = useNavigate();
 
   const handleDelete = async () => {
@@ -15,6 +18,15 @@ const MovieDetails = () => {
       console.error('Error deleting the movie', error);
     }
   };
+
+  const handleRatingSubmit = async (rating) => {
+    try {
+      await rateMovie(id, rating);
+      setUserRating(rating);
+    } catch (error) {
+      console.error('Error submitting movie rating', error);
+    }
+  }
 
   useEffect(() => {
     getMovie(id)
@@ -32,16 +44,46 @@ const MovieDetails = () => {
   }
 
   return (
-    <div>
-      <h1>{movie.title}</h1>
-      <p>Release Date: {movie.release_date}</p>
-      <p>Genre: {movie.genre}</p>
-      <p>Runtime: {movie.runtime}</p>
-      <p>Parental Rating: {movie.parental_rating}</p>
-      <p>Plot: {movie.plot}</p>
-      <br />
-      <button onClick={handleDelete}>Delete</button>
-    </div>
+    <Container>
+      <Row>
+        <Card>
+          <CardBody>
+            <CardTitle>{movie.title}</CardTitle>
+            <CardText>
+              <ListGroup>
+                <ListGroup.Item>Release Date: {movie.release_date}</ListGroup.Item>
+                <ListGroup.Item>Genre: {movie.genre}</ListGroup.Item>
+                <ListGroup.Item>Runtime: {movie.runtime}</ListGroup.Item>
+                <ListGroup.Item>Parental Rating: {movie.parental_rating}</ListGroup.Item>
+                <ListGroup.Item>Plot: {movie.plot}</ListGroup.Item>
+                <ListGroup.Item>Rating: {movie.rating}</ListGroup.Item>
+
+              <ListGroup.Item>{userRating === null ? (
+                <div>
+                  <p>Your Rating:</p>
+                  <div>
+                  {[1, 2, 3, 4, 5].map((rating) => (
+                    <span
+                      key={rating}
+                      onClick={() => handleRatingSubmit(rating)}
+                    >
+                      ★
+                    </span>
+                  ))}
+                  </div>
+                </div>
+              ) : (
+                <p>Your Rating: {userRating} ★</p>
+              )}</ListGroup.Item>
+              </ListGroup>
+              <br />
+              <Button variant="secondary" as={Link} to="/">Back</Button>
+              <Button variant="danger" onClick={handleDelete}>Delete</Button>
+            </CardText>
+          </CardBody>
+        </Card>
+      </Row>
+    </Container>
   );
 };
 
--- movies-app/src/services/api.js
+++ movies-app/src/services/api.js
@@ -17,6 +17,9 @@ const getMovie = (id) => api.get(`/movies/${id}`);
 const addMovie = (data) => api.post('/movies', data);
 const deleteMovie = (id) => api.delete(`/movies/${id}`);
 const getStats = (endpoint) => api.get(endpoint);
+const rateMovie = (movieId, rating) => {
+  return api.post(`/ratings/${movieId}`, { rating });
+};
 
 export {
   getMovies,
@@ -24,5 +27,6 @@ export {
   getStats,
   addMovie,
   deleteMovie,
+  rateMovie,
   STATS_ENDPOINTS,
 };
